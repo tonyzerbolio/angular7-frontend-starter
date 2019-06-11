b27d3eeed1143ab8f2c0f42cbcce13af84a9a45c
Anthony Zerbolio <anthony.zerbolio@unisys.com>
2019-06-11 10:32:58 -0500

    Added Okta sign-in widget to auth component

--- sample_app/src/app/auth/auth.component.ts
+++ sample_app/src/app/auth/auth.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationStart } from '@angular/router';
 
+// Okta
+import { OktaAuthService } from '@okta/okta-angular';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+
+// Conduit
 import { Errors, UserService } from '../core';
 
 @Component({
@@ -9,19 +14,40 @@ import { Errors, UserService } from '../core';
   templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
+  // Okta
+  widget = new OktaSignIn({
+    baseUrl: 'https://dev-167188.okta.com'
+  });
+
+  // Conduit
   authType = '';
   title = '';
   errors: Errors = {errors: {}};
   isSubmitting = false;
   authForm: FormGroup;
 
+  // Okta = oktaAuth: OktaAuthService
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private oktaAuth: OktaAuthService
   ) {
-    // use FormBuilder to create a form group
+    // Okta
+    router.events.forEach(event => {
+      if (event instanceof NavigationStart) {
+        switch(event.url) {
+          case '/login':
+            break;
+          default:
+            this.widget.remove();
+            break;
+        }
+      }
+    });
+
+    // Conduit - use FormBuilder to create a form group
     this.authForm = this.fb.group({
       'email': ['', Validators.required],
       'password': ['', Validators.required]
@@ -29,6 +55,22 @@ export class AuthComponent implements OnInit {
   }
 
   ngOnInit() {
+    // Okta
+    this.widget.renderEl({
+      el: '#okta-signin-container'},
+      (res) => {
+        if (res.status === 'SUCCESS') {
+          this.oktaAuth.loginRedirect('/', { sessionToken: res.session.token });
+          // Hide the widget
+          this.widget.hide();
+        }
+      },
+      (err) => {
+        throw err;
+      }
+    );
+
+    // Conduit
     this.route.url.subscribe(data => {
       // Get the last piece of the URL (it's either 'login' or 'register')
       this.authType = data[data.length - 1].path;
