75808e37f9b1cf9d1270815e92d7717594ba8b13
Anthony Zerbolio <anthony.zerbolio@unisys.com>
2019-06-20 11:11:10 -0500

    Added login to detect login status to show/hide nav items

--- sample_app/src/app/shared/layout/header.component.ts
+++ sample_app/src/app/shared/layout/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-layout-header',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private oauthService: OAuthService) { }
+
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+  
+  get givenName() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    }
+    return claims['name'];
+  }
 
   ngOnInit() { }
 
