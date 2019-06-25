0a2659fb0486205c27e0727a0554a3147a2cc132
Anthony Zerbolio <anthony.zerbolio@unisys.com>
2019-06-25 14:21:36 -0500

    discete service uri vars and K8s url update

--- sample_app/src/environments/environment.prod.ts
+++ sample_app/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
+// IMPORTANT - The values below need to be updated to properly reflect the K8s environment
+// For ng build --env=prod use this pattern: http://servicename.namespace:port
+// http://service1.comet:8092 // << For Prod build
 export const environment = {
   production: true,
-  api_url: 'https://conduit.productionready.io/api',
-  customers_url: 'http://localhost:8090',
-  service1_url: 'http://localhost:8081'
+  service1_url: 'http://service1.comet',
+  service1_port: ':8092',
+  service1_str: '/customers'
 };
