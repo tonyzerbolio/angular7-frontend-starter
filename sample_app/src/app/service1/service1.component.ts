f965a0173325f681d9e2a1ad81d8e7e516ac3b60
Anthony Zerbolio <anthony.zerbolio@unisys.com>
2019-07-09 15:31:48 -0500

    Added list/grid switch, basic crud screen, updated styles

--- sample_app/src/app/service1/service1.component.ts
+++ sample_app/src/app/service1/service1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Service1ApiService } from "../core/services/http-api.service";
 import { environment } from '../../environments/environment';
+import { Customer } from '../core/models/customer.model';
 
 @Component({
   selector: 'app-service1',
@@ -9,14 +10,30 @@ import { environment } from '../../environments/environment';
 })
 export class Service1Component implements OnInit {
 
-  Customer: any = [];
+  Customers: any = [];
   ServiceURL = `${environment.service1_url}`;
   ServicePORT = `${environment.service1_port}`;
   ServiceString = `${environment.service1_str}`;
+
+  selectedCustomer: Customer;
+
+  list = false;
   
   constructor(
     public svcApi: Service1ApiService
   ) { }
+  
+  onSelect(customer: Customer): void {
+    this.selectedCustomer = customer;
+  }
+
+  toggleList(): void {
+    if ( !this.list ) {
+      this.list = true;
+    } else {
+      this.list = false;
+    }
+  }
 
   ngOnInit() {
     this.getData()
@@ -24,7 +41,7 @@ export class Service1Component implements OnInit {
  
   getData() {
     return this.svcApi.getService1(this.ServiceURL + this.ServicePORT, this.ServiceString).subscribe((data: {}) => {
-        this.Customer = data;
+        this.Customers = data;
     })
   }
 
